ad88d21e8a9e95ef0abd40951f61203c050e4d75
Will Weber <rwaweber@udel.edu>
2014-08-26 15:48:15 -0400

    Will's Test Commit!
    Testing the commit; added a print line with greeting.

--- CTAEMSTaskGeneration/src/HelloCisc366.java
+++ CTAEMSTaskGeneration/src/HelloCisc366.java
@@ -7,6 +7,7 @@ public class HelloCisc366 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, Cisc366-Matt");
+		System.out.println("Hey, Cisc366-Will!");
 	}
 
 }
