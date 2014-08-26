5bd0bbdfd3adf7a3693b684cc885aa057f24c83d
mattsap <mattsap@udel.edu>
2014-08-26 11:04:49 -0400

    Initial Commit for Introductions

---
+++ CTAEMSTaskGeneration/src/HelloCisc366.java
@@ -0,0 +1,12 @@
+
+public class HelloCisc366 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Cisc366-Matt");
+	}
+
+}
